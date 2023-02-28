cc1282bfd56489714545bba61d8d49612feafe7e
Olga <olga1998senko@gmail.com>
2023-02-28 14:57:46 +0300

    main project

---
+++ script/script.js
@@ -0,0 +1,17 @@
+"use strict";
+//Task 1
+let admin, name;
+name = "John";
+admin = name;
+alert(admin);
+
+//Task 2
+let ourPlanetName, currentUserName;
+ourPlanetName = "Earth";
+currentUserName = "Olga";
+
+//Task 3
+let a, b;
+a = "10";
+b = 10;
+console.log(a+b);
